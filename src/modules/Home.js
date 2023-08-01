59a14ab43903fca366bccf25c325929d7410eab0
VIKAS VERMA <vikasvermaupvns@gmail.com>
2023-08-01 08:00:41 -0400

    Initial commit

---
+++ src/modules/Home.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Hero from '../components/Hero';
+import Products from '../components/Products';
+import FeaturedCard from '../components/FeaturedCard';
+import StatCard from '../components/StatCard';
+import Footer from '../components/Footer';
+import { useEffect } from 'react';
+
+
+const Home = () => {
+
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        const fetchProducts = async ()=>{
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+            console.log(data);
+            setProducts(data);
+        }
+        fetchProducts();
+    }, [])
+
+    return (
+        <>
+            <Hero />
+            <div className="flex flex-col text-center w-full mb-20">
+                <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PRODUCTS</h2>
+                <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Most Popular Product's</h1>
+            </div>
+            {
+                products.length > 0 ? <Products products={products}/>:<div>Loading.....</div>
+            }
+            <Products />
+            <FeaturedCard />
+            <StatCard/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home
